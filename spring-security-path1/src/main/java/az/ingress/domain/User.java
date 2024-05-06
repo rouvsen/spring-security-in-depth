d955d021fdc20b84e927744dbc263c5eb8410923
rouvsen <rouvsen@gmail.com>
2024-05-07 00:28:45 +0400

    add User Domain and Role basics

---
+++ spring-security-path1/src/main/java/az/ingress/domain/User.java
@@ -0,0 +1,41 @@
+package az.ingress.domain;
+
+import az.ingress.domain.role.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private Set<Role> authorities;
+
+    private boolean accountNonExpired;
+    private boolean isEnabled;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+
+}
