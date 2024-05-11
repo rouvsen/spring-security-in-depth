2e2ccab15b316bb3777089618a329fffb4def3ee
rouvsen <rouvsen@gmail.com>
2024-05-11 21:21:47 +0400

    add custom user details service "within the fake context like"

---
+++ spring-security-path2/src/main/java/az/ingress/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package az.ingress.service;
+
+import az.ingress.model.User;
+import az.ingress.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUserByEmail(email);
+        List<String> roles = new ArrayList<>();
+        roles.add("USER");
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(roles.toArray(new String[0]))
+                .build();
+    }
+}
